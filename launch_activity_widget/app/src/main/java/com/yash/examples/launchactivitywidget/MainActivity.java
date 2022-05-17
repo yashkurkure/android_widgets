4313b922f1879be0eb7a557f2c8fa34abd79f58b
yashkurkure <yash.kurkure1903@gmail.com>
2022-05-17 16:03:05 -0700

    Added Log message for onCreate

--- launch_activity_widget/app/src/main/java/com/yash/examples/launchactivitywidget/MainActivity.java
+++ launch_activity_widget/app/src/main/java/com/yash/examples/launchactivitywidget/MainActivity.java
@@ -3,12 +3,16 @@ package com.yash.examples.launchactivitywidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "Started onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
